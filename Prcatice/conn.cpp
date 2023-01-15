55f5a22b54399fea3952f91c3eab48e258130a42
Nishu Kumari <93523544+Nishu2003@users.noreply.github.com>
2023-01-15 19:21:43 +0530

    Create conn.cpp

---
+++ Prcatice/conn.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n;
+	    cin>>n;
+	    
+	    int k=0;
+	    
+	    if(n<7 && n%2!=0)
+	    cout<<"NO"<<endl;
+	    else
+	    cout<<"YES"<<endl;
+	}
+	return 0;
+}
