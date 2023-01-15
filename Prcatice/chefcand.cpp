7b7408f7b5b6882387a308f1e76c7f731c640d5a
Nishu Kumari <93523544+Nishu2003@users.noreply.github.com>
2023-01-15 19:23:14 +0530

    Create chefcand.cpp

---
+++ Prcatice/chefcand.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    
+	    if(x>=n)
+	    cout<<0<<endl;
+	    else if((n-x)%4==0)
+	    cout<<(n-x)/4<<endl;
+	    else
+	    cout<<((n-x)/4)+1<<endl;
+	}
+	return 0;
+}
